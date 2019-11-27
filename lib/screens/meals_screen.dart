4a475be6c3b2fa4eead704d94113c324e1c9fd9b
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-11-27 11:15:47 +0100

    Add meals data

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myfoodz/models/dummy_data.dart';
 
 class MealsScreen extends StatelessWidget {
   static const String id = 'meals';
@@ -7,14 +8,21 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
     final categoryName = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryName),
       ),
-      body: Center(
-        child: Text('Yummy!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }

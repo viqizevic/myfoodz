681c90f87f9dc5be8f3d27a9a21c3169c8a0c822
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-11-27 12:39:32 +0100

    Add meal item widget

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myfoodz/models/dummy_data.dart';
+import 'package:myfoodz/models/meal.dart';
+import 'package:myfoodz/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   static const String id = 'meals';
@@ -20,7 +22,14 @@ class MealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          Meal meal = categoryMeals[index];
+          return MealItem(
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            complexity: meal.complexity,
+            affordability: meal.affordability,
+          );
         },
         itemCount: categoryMeals.length,
       ),

38a9043bb87d5caacb3a65a69debf7d5280604a3
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-11-28 10:45:39 +0100

    Add main drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myfoodz/screens/categories_screen.dart';
 import 'package:myfoodz/screens/favorites_screen.dart';
+import 'package:myfoodz/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -33,6 +34,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
